9707873eb2b7cb2ecc7035ee8ddc51e021f15cc6
Matthew J. Berger <matthewberger@nevada.unr.edu>
2018-12-30 23:04:28 -0800

    Reexport types from core.

--- src/bin/pong.rs
+++ src/bin/pong.rs
@@ -1,27 +1,18 @@
-extern crate sdl2;
-extern crate gl;
 extern crate core;
+extern crate gl;
+extern crate sdl2;
 
-use core::{
-    app::Application2D
-};
-
-use sdl2::{
-    render::Canvas,
-    video::Window,
-};
+use core::app::{Application2D, Canvas, Color, Window};
 
 fn main() {
-   Application2D::run(handle_events, update, render);
+    Application2D::run(handle_events, update, render);
 }
 
-fn handle_events(_event: sdl2::event::Event) {
-}
+fn handle_events(_event: sdl2::event::Event) {}
 
-fn update() {
-}
+fn update() {}
 
 fn render(canvas: &mut Canvas<Window>) {
-    canvas.clear();
-    canvas.present();
+    // Change draw color to white
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
 }
